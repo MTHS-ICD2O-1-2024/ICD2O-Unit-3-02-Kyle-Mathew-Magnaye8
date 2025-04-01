6fe868af657d1911e27e198472db7b44bb580219
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-04-01 17:04:28 +0000

    added work in JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 kyle.matthew.magnaye All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: kyle.matthew.magnaye
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+function calculateVolume() {
+  const LENGTH_OF_PYRAMID = parseFloat(document.getElementById("length-of-pyramid").value)
+  const WIDTH_OF_PYRAMID = parseFloat(document.getElementById("width-of-pyramid").value)
+  const HEIGHT_OF_PYRAMID = parseFloat(document.getElementById("height-of-pyramid").value)
+
+  // Correct volume calculation
+  const volume = (LENGTH_OF_PYRAMID * WIDTH_OF_PYRAMID * HEIGHT_OF_PYRAMID) / 3
+
+  // Display result
+  document.getElementById("user-info").innerText = "Volume is: " + calculateVolume.toFixed(2) + " mm³"
+}
