37f82f86ec80df7ac23655ebc1f90184c6a9225d
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-04-02 16:48:22 +0000

    fixed java script

--- js/script.js
+++ js/script.js
@@ -10,27 +10,10 @@ function calculateVolume() {
   let widthInput = document.getElementById("width-of-pyramid").value
   let heightInput = document.getElementById("height-of-pyramid").value
 
-  // Convert to numbers
-  const LENGTH_OF_PYRAMID = Number(lengthInput)
-  const WIDTH_OF_PYRAMID = Number(widthInput)
-  const HEIGHT_OF_PYRAMID = Number(heightInput)
-
-  // Validate input
-  if (
-    isNaN(LENGTH_OF_PYRAMID) ||
-    isNaN(WIDTH_OF_PYRAMID) ||
-    isNaN(HEIGHT_OF_PYRAMID)
-  ) {
-    document.getElementById("user-info").innerText =
-      "Please enter valid numbers for all fields."
-    return
-  }
-
   // Correct volume calculation
-  const volume = (LENGTH_OF_PYRAMID * WIDTH_OF_PYRAMID * HEIGHT_OF_PYRAMID) / 3
+  const calculateVolume = (lengthInput *widthInput * heightInput) / 3
 
   // Display result
   document.getElementById("user-info").innerText =
-    "Volume is: " + volume.toFixed(2) + " mm³"
-  componentHandler.upgradeDom() 
+    "Volume is: " + calculateVolume.toFixed(2) + " mm³"
 }
